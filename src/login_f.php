f63f655263caa36f6cf663956b83a45740458b42
Yoan Martin <yoan.martin@epfl.ch>
2020-07-01 14:49:25 +0200

    Change paths

--- src/login_f.php
+++ src/login_f.php
@@ -7,7 +7,7 @@
     * This software may be modified and distributed under the terms
     * of the MIT license.  See the LICENSE file for details.
     */
-    require('../vendor/autoload.php');
+    require_once __DIR__ . '/../../../autoload.php';
 
     use Webauthn\PublicKeyCredentialRequestOptions;
     use Webauthn\PublicKeyCredentialUserEntity;
